aa87e58f77dafef3467991a6eb485be6b8fa5e20
ilke-kartal <113463171+ilke-kartal@users.noreply.github.com>
2022-10-15 15:16:16 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace PasswordChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.passlbl = new System.Windows.Forms.Label();
+            this.passtxt = new System.Windows.Forms.TextBox();
+            this.annlbl = new System.Windows.Forms.Label();
+            this.passStrMeter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // passlbl
+            // 
+            this.passlbl.AutoSize = true;
+            this.passlbl.BackColor = System.Drawing.Color.Transparent;
+            this.passlbl.Location = new System.Drawing.Point(59, 148);
+            this.passlbl.Name = "passlbl";
+            this.passlbl.Size = new System.Drawing.Size(81, 20);
+            this.passlbl.TabIndex = 0;
+            this.passlbl.Text = "Yeni Şifre : ";
+            // 
+            // passtxt
+            // 
+            this.passtxt.Location = new System.Drawing.Point(137, 145);
+            this.passtxt.Name = "passtxt";
+            this.passtxt.Size = new System.Drawing.Size(245, 27);
+            this.passtxt.TabIndex = 1;
+            this.passtxt.TextChanged += new System.EventHandler(this.passtxt_TextChanged);
+            // 
+            // annlbl
+            // 
+            this.annlbl.Location = new System.Drawing.Point(12, 9);
+            this.annlbl.Name = "annlbl";
+            this.annlbl.Size = new System.Drawing.Size(463, 101);
+            this.annlbl.TabIndex = 2;
+            // 
+            // passStrMeter
+            // 
+            this.passStrMeter.Location = new System.Drawing.Point(55, 128);
+            this.passStrMeter.Name = "passStrMeter";
+            this.passStrMeter.Size = new System.Drawing.Size(341, 72);
+            this.passStrMeter.TabIndex = 3;
+            this.passStrMeter.Text = "Lütfen sifrenizi giriniz.";
+            this.passStrMeter.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(487, 274);
+            this.Controls.Add(this.passtxt);
+            this.Controls.Add(this.passlbl);
+            this.Controls.Add(this.annlbl);
+            this.Controls.Add(this.passStrMeter);
+            this.Name = "Form1";
+            this.Text = "Password Change";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label passlbl;
+        private TextBox passtxt;
+        private Label annlbl;
+        private Label passStrMeter;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Forms.VisualStyles;
+
+namespace PasswordChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void passtxt_TextChanged(object sender, EventArgs e)
+        {
+            string pass;
+            string lowerCaseRegex = "[a-z]";
+            string upperCaseRegex = "[A-Z]";
+            string digitRegex = "[0-9]";
+            string spacialCharsRegex = "^[^<>{}\"/|;:.,~!?@#$%^=&*\\]\\\\()\\[¿§«»ω⊙¤°℃℉€¥£¢¡®©_+]*$";
+            pass = passtxt.Text;
+            string ann = "";
+            int control = 0;
+
+            if (passtxt.Text == "")
+            {
+                annlbl.Text = "";
+            } else {
+                string lastCharacter = pass.Substring(pass.Length - 1); // Girilen son karakteri kontrol etmek için, son karakteri bir değişkene atıyoruz.
+
+                if (lastCharacter == " ")
+                {
+                    pass = pass.Substring(0, pass.Length - 1); // Son karakter boşluk ise, son karakteri silip yeni şifre değerimizi oluşturuyoruz.
+                    passtxt.Text = pass; // Yeni şifre değerimizi textboxımızın text 'ine atıyoruz.
+                    passtxt.SelectionStart = passtxt.Text.Length; // Textbox değerini yeniden oluşturduğumuz için, imleç en başa geliyor. İmleci yeniden sona getirmek için kullanıyoruz.
+                }
+
+                if (passtxt.Text.Length >= 5)
+                {
+
+                    ann = "Şifre içerisinde";
+
+                    if (!Regex.Match(pass, lowerCaseRegex).Success)
+                    {
+                        ann += " en az bir tane küçük harf,";
+                        control += 1;
+                    }
+
+                    if (!Regex.Match(pass, upperCaseRegex).Success)
+                    {
+                        ann += " en az bir tane büyük harf,";
+                        control += 1;
+                    }
+
+                    if (!Regex.Match(pass, digitRegex).Success)
+                    {
+                        ann += " en az bir tane rakam,";
+                        control += 1;
+                    }
+
+                    if (Regex.Match(pass, spacialCharsRegex).Success)
+                    {
+                        ann += " en az bir özel karakter,";
+                        control += 1;
+                    }
+
+                    if (control > 0)
+                    {
+                        ann = ann.Remove(ann.Length - 1);
+                        annlbl.Text = ann + " olmalı.";
+
+                        switch (control)
+                        {
+                            case 1:
+                                passStrMeter.BackColor = Color.GreenYellow;
+                                passlbl.BackColor = Color.GreenYellow;
+                                passStrMeter.Text = "Güçlü";
+                                break;
+                            case 2:
+                                passStrMeter.BackColor = Color.Yellow;
+                                passlbl.BackColor = Color.Yellow;
+                                passStrMeter.Text = "Normal";
+                                break;
+                            case 3:
+                                passStrMeter.BackColor = Color.Orange;
+                                passlbl.BackColor = Color.Orange;
+                                passStrMeter.Text = "Güçsüz";
+                                break;
+                            case 4:
+                                passStrMeter.BackColor = Color.Red;
+                                passlbl.BackColor = Color.Red;
+                                passStrMeter.Text = "Çok Güçsüz";
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        annlbl.Text = "Şifreniz uygundur.";
+                        passStrMeter.BackColor = Color.Green;
+                        passlbl.BackColor = Color.Green;
+                        passStrMeter.Text = "Çok Güçlü";
+                    }
+                } else
+                {
+                    annlbl.Text = "Şifreniz 5 karakterden kısa olamaz.";
+                }
+
+            }
+
+
+        }
+    }
+}
